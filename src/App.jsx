8acc95d7f656e8232bb4a6147c2d6379f683ad87
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-06-06 01:04:37 +0530

    complete routing of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import FooterComp from "./components/footer/FooterComp";
+import Home from "./pages/home/Home";
+import CreateTodo from "./pages/createTodo/CreateTodo";
+import ViewTodo from "./pages/viewTodo/ViewTodo";
+import EditDelTodo from "./pages/edit&delTodo/EditDelTodo";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/createtodo' element={<CreateTodo />} />
+        <Route path='/viewtodo' element={<ViewTodo />} />
+        <Route path='/edit-deltodo' element={<EditDelTodo />} />
+      </Routes>
+      <FooterComp />
+    </BrowserRouter>
+  );
 }
 
 export default App;
