e5c21138800db4b3f8d1c4396195ffbea42d4abb
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-06-05 23:29:07 +0530

    Install framer-motion and moving border

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 const flowbite = require("flowbite-react/tailwind");
+const flattenColorPalette =
+  require("tailwindcss/lib/util/flattenColorPalette").default;
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin()],
+  plugins: [flowbite.plugin(), addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
