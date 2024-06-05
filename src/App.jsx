96f462efeee860b79f7dd0c74ef33c4edce0e281
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-06-05 22:53:16 +0530

    setUp flowbite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from "flowbite-react";
 import React from "react";
 
 function App() {
   return (
     <div className='flex justify-center items-center'>
       <h1 className='font-bold text-blue-700 text-2xl'>Chandan</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
